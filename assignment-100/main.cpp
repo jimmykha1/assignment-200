487532464ea837197f2b826c720705e666b019c2
jimmykha1 <jimmykha@outlook.com>
2022-10-14 22:52:59 -0500

    Initial Commit

---
+++ assignment-100/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment-100
+//
+//  Created by Jimmy Kha on 10/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
